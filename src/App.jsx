5bb927e22909416c5e64ce2731a6e1bf49db63db
bishwa <108havocx108@gmail.com>
2025-07-03 20:58:00 +0545

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import HeroSection from "./components/HeroSection";
+import Products from "./components/Products";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <HeroSection />
+      <Products />
+    </div>
+  );
 };
 
 export default App;
