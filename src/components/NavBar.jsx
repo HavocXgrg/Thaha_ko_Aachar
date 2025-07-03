c64278dde17577e6aa2e7d8862a993521f91f9b5
bishwa <108havocx108@gmail.com>
2025-07-03 20:56:35 +0545

    some changes on positioning and still to modify

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,25 +6,24 @@ import { RxCross2 } from "react-icons/rx";
 
 const NavBar = () => {
   return (
-    <div className="w-full max-w-[90%] sm:max-w-[80%] md:max-w-[72%] max-h-[58px] sm:max-h-[78px] bg-white rounded-full mt-6 md:mt-10 mx-auto relative flex items-center justify-center">
-      <div className=" flex items-center justify-around w-full ">
-        <div className="left-items">
+    <motion.div className="relative flex items-center justify-center max-w-[90%] sm:max-w-[80%] md:max-w-[72%] max-h-[58px] sm:max-h-[78px] bg-white rounded-full  mx-auto top-8 ">
+      <motion.div className=" flex items-center justify-around w-full h-full  ">
+        <motion.div className="left-items">
           {/* Hamburger menu toggles for mobile-devices */}
-          <motion.div className=" lg:hidden">
+          <motion.div className=" lg:hidden text-xl ">
             <CiMenuBurger />
           </motion.div>
           <div></div>
-        </div>
+        </motion.div>
 
-        <img
-          className="  max-w-22 sm:max-w-34 mt-1 ml-2 cursor-pointer overflow-hidden"
+        <motion.img
+          className=" h-auto max-w-22 sm:max-w-34 mt-1 ml-12 cursor-pointer "
           src={logo}
-          alt=""
+          alt="brand-logo"
         />
-
         <div className="item2 w-20 h-5 bg-yellow-400"></div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
