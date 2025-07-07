81e7c788fe577f1ecece4127929eb29ba962bc1d
bishwa <108havocx108@gmail.com>
2025-07-07 13:13:42 +0545

    added carousel comp and passed data via child props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import Products from "./components/Products";
+import Carousel from "./components/Carousel";
+import { TopProduct } from "./Constant/Data";
 
 const App = () => {
   return (
     <div>
       <HeroSection />
+      <Carousel images={TopProduct} />{" "}
+      {/* img is array of objs passed as a child prop to Carousel component*/}
       <Products />
     </div>
   );
