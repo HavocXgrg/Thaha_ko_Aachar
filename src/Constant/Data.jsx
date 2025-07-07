0217723bbd84b0abadbea67e0acad63e0cdb2c32
bishwa <108havocx108@gmail.com>
2025-07-07 13:14:44 +0545

    updated data

--- src/Constant/Data.jsx
+++ src/Constant/Data.jsx
@@ -1,1 +1,28 @@
-export const Data = [{}];
+import akabareImg from "../assets/product-img/akabare.png";
+import buffImg from "../assets/product-img/buff.png";
+import chickenImg from "../assets/product-img/chicken.png";
+import kauliImg from "../assets/product-img/kauli.png";
+import lapsiImg from "../assets/product-img/lapsi.png";
+import lasunImg from "../assets/product-img/lasun.png";
+import methiImg from "../assets/product-img/methi.png";
+import mixImg from "../assets/product-img/mix.png";
+import mulaImg from "../assets/product-img/mula.png";
+
+export const Img = [
+  { image: akabareImg, title: "Aakabare Aachar" },
+  { image: buffImg, title: "Buff Aachar" },
+  { image: chickenImg, title: "Chicken Aachar" },
+  { image: kauliImg, title: "Kauli Aachar" },
+  { image: lapsiImg, title: "Lapsi Aachar" },
+  { image: lasunImg, title: "Lasun Aachar" },
+  { image: methiImg, title: "Methi Aachar" },
+  { image: mixImg, title: "Mix Aachar" },
+  { image: mulaImg, title: "Mula Aachar" },
+];
+
+export const TopProduct = [
+  { image: chickenImg, title: "Chicken Aachar" },
+  { image: buffImg, title: "Buff Aachar" },
+  { image: lapsiImg, title: "Lapsi Aachar" },
+  { image: mixImg, title: "Mix Aachar" },
+];
