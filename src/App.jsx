d62f3f7d29f34773e3c9a5ceb01e6cf72e528059
bishwa <108havocx108@gmail.com>
2025-07-08 16:52:36 +0545

    updated some import files and names

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import NavBar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import Products from "./components/Products";
 import Carousel from "./components/Carousel";
-import { TopProduct } from "./Constant/Data";
+import { TopProduct, Product } from "./Constant/Data";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -11,7 +12,8 @@ const App = () => {
       <HeroSection />
       <Carousel images={TopProduct} />{" "}
       {/* img is array of objs passed as a child prop to Carousel component*/}
-      <Products />
+      <Products data={Product} />
+      <Footer />
     </div>
   );
 };
