1b966bf573c123feebb451e6dbf060fb264cb2b7
bishwa <108havocx108@gmail.com>
2025-07-22 13:56:11 +0545

    stats file for about section added

---
+++ src/Constant/AboutStats.js
@@ -0,0 +1,66 @@
+import noPreservative from "../assets/aboutsectionIMG/no-preservatives.png";
+import handcrafted from "../assets/aboutsectionIMG/handcrafted.png";
+import healthy from "../assets/aboutsectionIMG/healthy.png";
+import oil from "../assets/aboutsectionIMG/oil.png";
+import drying from "../assets/aboutsectionIMG/drying.png";
+
+export const stats = [
+  {
+    end: 10,
+    duration: 5,
+    text: "Years of Experience in Traditional Pickle Handcrafting",
+    plus: true,
+  },
+  {
+    end: 12,
+    duration: 5,
+    text: "Authentic Pickles Flavors",
+    plus: true,
+  },
+  {
+    end: 9,
+    duration: 5,
+    text: "Customer Satisfaction Ratings & Recommendations",
+    plus: true,
+  },
+  {
+    end: 80,
+    duration: 4,
+    text: "Local Farmers Sourced From, Supporting Sustainable Practices",
+    plus: true,
+  },
+];
+
+export const cardData = [
+  {
+    id: 1,
+    img: healthy,
+    title: "Healthy",
+    description: "Prepared with locally sourced farm fresh ingredients.",
+  },
+  {
+    id: 2,
+    img: noPreservative,
+    title: "Preservative-Free",
+    description:
+      "100% natural with no artificial preservatives, colors or flavours",
+  },
+  {
+    id: 3,
+    img: handcrafted,
+    title: "Handcrafted",
+    description: "At Home, with love, using our special recipes",
+  },
+  {
+    id: 4,
+    img: drying,
+    title: "Aged to Perfection",
+    description: "Sundried for up to 10-20 days For superior taste and quality",
+  },
+  {
+    id: 5,
+    img: oil,
+    title: "Cold Pressed Oil",
+    description: "Made in 100% Cold Pressed Mustard Oil",
+  },
+];
