0b8b5eb96b140cfa545335ecdf39f6522e287e5b
bishwa <108havocx108@gmail.com>
2025-07-22 23:00:01 +0545

    initial commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="h-screen">Footer</div>;
+  return <div className="min-h-[80vh] bg-green-900  ">Footer</div>;
 };
 
 export default Footer;
